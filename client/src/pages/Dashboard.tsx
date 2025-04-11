fe4aa51fb900ffa05ddbda2be9c6e159f5bc9e48
Gabriella <gabriellarosario72@gmail.com>
2025-04-11 03:32:50 -0400

    add: Dashboard

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+import DashboardDoctor from '../components/Doctor/Dashboard';
+
+export default function DoctorDashboard() {
+  return (
+    <div className="min-h-screen bg-gray-100 px-4 py-8">
+      <DashboardDoctor />
+    </div>
+  );
+}
