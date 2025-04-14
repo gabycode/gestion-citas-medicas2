30f7c34520d2182f6076a6911f0ebd08d611ea5b
Gabriella <gabriellarosario72@gmail.com>
2025-04-14 02:24:45 -0400

    fix: ui login component

--- client/src/components/Doctor/Login.tsx
+++ client/src/components/Doctor/Login.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { loginDoctor } from "../../services/doctoresService";
 import { useState } from "react";
+import { ArrowLeftIcon } from "lucide-react";
+import doctorTablet from "../../assets/doctor-tablet.png";
 
 export default function LoginDoctor() {
   const {
@@ -33,69 +35,90 @@ export default function LoginDoctor() {
   const inputStyle =
     "w-full px-3 py-2 border rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500";
   const buttonStyle =
-    "bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-200 w-full";
+    "bg-blue-600 text-white px-4 py-2 my-4 rounded hover:bg-blue-700 transition duration-200 w-full";
   const labelStyle = "block text-sm font-medium text-gray-700 mb-1";
   const errorStyle = "text-red-500 text-sm mt-1";
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-sm mx-auto mt-10 space-y-4 bg-white p-6 rounded-lg shadow-md"
-    >
-      <h2 className="text-2xl font-bold text-center text-blue-700 mb-4">
-        Iniciar Sesión - Doctor
-      </h2>
-
-      <div>
-        <label className={labelStyle}>Correo electrónico</label>
-        <input
-          {...register("email", {
-            required: "El correo es obligatorio",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Correo inválido",
-            },
-          })}
-          type="email"
-          className={inputStyle}
-        />
-        {errors.email && (
-          <p className={errorStyle}>{errors.email.message?.toString()}</p>
-        )}
-      </div>
-
-      <div>
-        <label className={labelStyle}>Contraseña</label>
-        <input
-          {...register("password", {
-            required: "La contraseña es obligatoria",
-          })}
-          type="password"
-          className={inputStyle}
-        />
-        {errors.password && (
-          <p className={errorStyle}>{errors.password.message?.toString()}</p>
-        )}
-        <p
-          className="text-sm text-right text-blue-600 underline cursor-pointer mt-2"
-          onClick={() => navigate("/forgot-password")}
+    <div className="flex shadow-md">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="w-full bg-white p-6  md:rounded-l-lg md:w-1/2"
+      >
+        <div
+          className="flex items-center cursor-pointer gap-2"
+          onClick={() => navigate("/")}
         >
-          ¿Olvidaste tu contraseña?
-        </p>
-      </div>
+          <ArrowLeftIcon className="w-4 h-4" />
+          <p className="text-sm ">Volver al inicio</p>
+        </div>
 
-      <button type="submit" className={buttonStyle} disabled={loading}>
-        {loading ? "Iniciando..." : "Iniciar sesión"}
-      </button>
-      <p className="text-sm text-center text-black-600  mt-2">
-        ¿No tienes cuenta?{" "}
-        <span
-          onClick={() => navigate("/signup")}
-          className="underline cursor-pointer"
-        >
-          Regístrate aquí
-        </span>
-      </p>
-    </form>
+        <div className="h-full flex flex-col justify-center md:w-[80%] mx-auto">
+          <h2 className="text-3xl font-bold text-center text-blue-700">
+            Ingreso de Doctores
+          </h2>
+          <p className="text-sm text-center text-gray-600 mb-4">
+            Inicia sesión para acceder a tu panel de control
+          </p>
+          <div className="mb-4">
+            <label className={labelStyle}>Correo electrónico</label>
+            <input
+              {...register("email", {
+                required: "El correo es obligatorio",
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Correo inválido",
+                },
+              })}
+              type="email"
+              className={inputStyle}
+            />
+            {errors.email && (
+              <p className={errorStyle}>{errors.email.message?.toString()}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className={labelStyle}>Contraseña</label>
+            <input
+              {...register("password", {
+                required: "La contraseña es obligatoria",
+              })}
+              type="password"
+              className={inputStyle}
+            />
+            {errors.password && (
+              <p className={errorStyle}>
+                {errors.password.message?.toString()}
+              </p>
+            )}
+            <p
+              className="text-sm text-right text-blue-600 underline cursor-pointer mt-2"
+              onClick={() => navigate("/forgot-password")}
+            >
+              ¿Olvidaste tu contraseña?
+            </p>
+          </div>
+          <button type="submit" className={buttonStyle} disabled={loading}>
+            {loading ? "Iniciando..." : "Iniciar sesión"}
+          </button>
+          <p className="text-sm text-center text-black-600 py-2">
+            ¿No tienes cuenta?{" "}
+            <span
+              onClick={() => navigate("/signup")}
+              className="underline cursor-pointer"
+            >
+              Regístrate aquí
+            </span>
+          </p>
+        </div>
+      </form>
+      <div className="hidden md:block w-1/2  bg-[#01388f] overflow-hidden rounded-r-lg">
+        <img
+          src={doctorTablet}
+          alt="Doctors"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
   );
 }
